c2ca3a84c1e8a0ad61a35d9df2f1ada109557e11
willin <williamsilva1942@gmail.com>
2024-08-26 10:12:21 -0300

    feat: SEO improvment and revalidation

--- src/constants/metadata.ts
+++ src/constants/metadata.ts
@@ -4,33 +4,33 @@ export const defaultMetadata: Metadata = {
     title: 'WebWise',
     description:
       'A WebWise é uma assessoria de marketing digital especializada em impulsionar seu negócio. Nosso foco é ajudar você a vender mais, alcançar um público maior, aumentar a frequência das vendas e maximizar o valor de cada transação.',
-    icons: {
-      icon: [
-        {
-          url: '',
-          sizes: '32x32',
-          type: 'image/png',
-        },
-        {
-          url: '',
-          sizes: '16x16',
-          type: 'image/png',
-        },
-      ],
-      apple:
-        '',
-      other: [
-        {
-          rel: 'manifest',
-          url: '',
-        },
-        {
-          rel: 'mask-icon',
-          url: '',
-          color: '#E40613',
-        },
-      ],
-    },
+      icons: {
+        icon: [
+          {
+            url: 'https://cms.consigacred.com.br/uploads/favicon_32x32_06b3adb156.png',
+            sizes: '32x32',
+            type: 'image/png',
+          },
+          {
+            url: 'https://cms.consigacred.com.br/uploads/favicon_16x16_d541365afd.png',
+            sizes: '16x16',
+            type: 'image/png',
+          },
+        ],
+        apple:
+          'https://cms.consigacred.com.br/uploads/apple_touch_icon_c9ae0ef60b.png',
+        other: [
+          {
+            rel: 'manifest',
+            url: 'https://cms.consigacred.com.br/uploads/site_1830ddbc2c.webmanifest',
+          },
+          {
+            rel: 'mask-icon',
+            url: 'https://cms.consigacred.com.br/uploads/safari_pinned_tab_7b35ce65ec.svg',
+            color: '#E40613',
+          },
+        ],
+      },
     openGraph: {
       title: 'WebWise',
       description:
