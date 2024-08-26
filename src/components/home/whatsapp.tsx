a956033cfaa9673332e0f0d87ef39284e129e7df
willin <williamsilva1942@gmail.com>
2024-08-26 09:51:00 -0300

    feat: add whatsapp

--- src/components/home/whatsapp.tsx
+++ src/components/home/whatsapp.tsx
@@ -4,7 +4,7 @@ import { FaWhatsapp } from 'react-icons/fa';
 export const Whatsapp = () => {
   return (
     <a
-      href="https://wa.me/5511999999999"
+      href="https://wa.me/5516997260636"
       target="_blank"
       rel="noopener noreferrer"
       className="fixed bottom-4 left-4 bg-[rgb(22,156,18)] text-white rounded-full p-3 shadow-lg hover:bg-green-600 hover:shadow-xl hover:scale-105 hover:brightness-110 transition-all duration-300 ease-in-out"
