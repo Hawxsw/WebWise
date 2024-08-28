8849c0df861093d904e1a27d017f7467fca2a584
willin <williamsilva1942@gmail.com>
2024-08-28 10:28:54 -0300

    fix(accessibility): improve color contrast and link descriptions for better accessibility
    - Updated button and link colors to ensure sufficient contrast
    - Added descriptive aria-labels to improve link accessibility
    - Adjusted styles for better user experience and compliance with accessibility standards

--- src/components/shared/footer.tsx
+++ src/components/shared/footer.tsx
@@ -7,13 +7,13 @@ export const Footer = () => {
       <div className="text-white pb-[3%] leading-[160%] text-sm">
         © Copyright 2024 – WebWise | Developed by{' '}
         <Link
-          className="font-normal text-[#FF4C4C]" // Alterado para um vermelho mais claro
+          className="font-normal text-[#FF4C4C]"
           href="https://github.com/Hawxsw"
           target="_blank"
         >
           OmniSpark
         </Link>{' '}
-        & <b className="font-normal text-[#FF4C4C]">WebWise</b>. {/* Alterado para um vermelho mais claro */}
+        & <b className="font-normal text-[#FF4C4C]">WebWise</b>.
       </div>
     </div>
   );
